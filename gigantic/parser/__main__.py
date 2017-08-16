a0fa751b347ae4270f92fbb4af58b4724ef7da1b
Jordan Duty <djdduty@gmail.com>
2017-08-15 21:03:35 -0700

    Getting there

--- gigantic/parser/__main__.py
+++ gigantic/parser/__main__.py
@@ -3,70 +3,91 @@
 import os
 import configparser
 import tempfile
+import json
 
 
-class Resource(object): # Abstraction, no real sections in the ini
-	ResourceID = ''						# puAdept4_Cooldown_Upgrade, Adept, etc etc
+class Resource(dict): # Abstraction, no real sections in the ini
+	resourceid = None					# puAdept4_Cooldown_Upgrade, Adept, etc etc
+
+	def __repr__(self):
+		return self.resourceid
 
 
 class UIResource(Resource): # Abstraction, no real sections in the ini
-	UIResourceID = ''					# Adept, AdeptGhost, etc
+	uiresourceid = ''					# Adept, AdeptGhost, etc
 
 
-class Skill(Resource): # [ResourceID RxSkillProvider]
-	UIResourceID = ''					# Skill1, used as translation index?
-	HeroArchetypeName = ''				# Adept, same as HeroArchetypeName in Archetype
-	SkillName = ''						# Skill1, Skill2, Skill3, or Skill3 typically
+class Skill(UIResource): # [ResourceID RxSkillProvider]
+	heroarchetypename = ''				# Adept, same as HeroArchetypeName in Archetype
+	skillname = ''						# Skill1, Skill2, Skill3, or Skill3 typically
 
 
 class Upgrade(Resource): # Abstraction, no real sections in the ini
-	HeroName = ''						# Adept, same as HeroArchetypeName
-	UpgradeTier = ''					# ESUT_Upgrade1, ESUT_Upgrade1_SubUpgrade1, ESUT_Upgrade2, ESUT_Upgrade2_SubUpgrade1, ESUT_None, etc
-	MinHeroLevel = 0					# 1 or 5?
-	UpgradePathCategory = ''			# UPC_Offense, UPC_Defense, UPC_BurstDamage, UPC_Healing, UPC_Sustain, UPC_Mobility, UPC_AntiDebuffs
+	heroname = ''						# Adept, same as HeroArchetypeName
+	upgradetier = ''					# ESUT_Upgrade1, ESUT_Upgrade1_SubUpgrade1, ESUT_Upgrade2, ESUT_Upgrade2_SubUpgrade1, ESUT_None, etc
+	minherolevel = 0					# 1 or 5?
+	upgradepathcategory = ''			# UPC_Offense, UPC_Defense, UPC_BurstDamage, UPC_Healing, UPC_Sustain, UPC_Mobility, UPC_AntiDebuffs
 
 
 class SkillUpgrade(Upgrade): # [ResourceID RxSkillUpgradeProvider]
-	SkillUpgradeCategory = ''			# EUC_Skill1Upgrade where Skill1 is the 'SkillName' of the skill
-	SkillIndex = 0						# Not sure what it corresponds to, have seen anywhere from 11-34
+	skillupgradecategory = ''			# EUC_Skill1Upgrade where Skill1 is the 'SkillName' of the skill
+	skillindex = 0						# Not sure what it corresponds to, have seen anywhere from 11-34
 
 
 class PassiveUpgrade(Resource): # [ResourceID RxPassiveUpgradeProvider]
-	PassiveUpgradeCategory = '' 		# EUC_UnlockedDuringClash
-	PassiveIconIdentifier = ''			# AttackFocus
-	PassiveIndex = 0					# 4, 5, 6?
+	passiveupgradecategory = '' 		# EUC_UnlockedDuringClash
+	passiveiconidentifier = ''			# AttackFocus
+	passiveindex = 0					# 4, 5, 6?
+
+
+class UpgradePath(Resource): # [ResourceID RxUpgradePathProvider]
+	setname = ''						# up_Adept_Default
+	upgradetype = ''					# UPT_Skill1_U1
+	groupindex = 0						# 0 - 15 in increments of 1?
+
+	def __repr__(self):
+		return str(self.setname)
 
 
 class SummonProvider(UIResource): # [ResourceID RxSummonProvider]
-	ProviderPawnClassPath = ''			# "RxGameContent.RxPawn_AdeptAttacker"
-	SummonBehaviorTreeName = ''			# "BT_AdeptAttacker"
-	CreatorID = ''						# Adept (Maybe HeroArchetypeName?)
+	providerpawnclasspath = ''			# "RxGameContent.RxPawn_AdeptAttacker"
+	summonbehaviortreename = ''			# "BT_AdeptAttacker"
+	creatorid = ''						# Adept (Maybe HeroArchetypeName?)
 
 
 class Archetype(UIResource): # [HeroArchetypeName RxHeroProvider]
-	HeroArchetypeName = ''				# Adept_Pawn_Arch
-	ProviderPawnClassPath = ''			# "RxGameContent.RxPawn_Adept"
-	DataSortPriority = 1700				# 1700
-	PrimaryTraitPath = ''				# tp_Duelist
-	SecondaryTraitPath = ''				# tp_helper
-	AttackStat = 0						# 50
-	DefenseStat = 0						# 60
-	MobilityStat = 0					# 30
-	UtilityStat = 0						# 80
-	PlayDifficulty = 0					# 5
-	PlayStyle = 0						# 14
-	BaseUIReticleSpreadMultiplier = 1.0	# 1.0f
-	LowHealthTriggerPercent = 40.0		# 40.0f
-	LowHealthRecoveredPercent = 43.0	# 43.3f
-	LowStaminaTriggerPercent = 25.0		# 25.0f
-	LowStaminaRecoveredPercent = 30.0	# 30.0f
+	heroarchetypename = ''				# Adept_Pawn_Arch
+	providerpawnclasspath = ''			# "RxGameContent.RxPawn_Adept"
+	datasortpriority = 1700				# 1700
+	primarytraitpath = ''				# tp_Duelist
+	secondarytraitpath = ''				# tp_helper
+	attackstat = 0						# 50
+	defensestat = 0						# 60
+	mobilitystat = 0					# 30
+	utilitystat = 0						# 80
+	playdifficulty = 0					# 5
+	playstyle = 0						# 14
+	baseuireticlespreadmultiplier = 1.0	# 1.0f
+	lowhealthtriggerpercent = 40.0		# 40.0f
+	lowhealthrecoveredpercent = 43.0	# 43.3f
+	lowstaminatriggerpercent = 25.0		# 25.0f
+	lowstaminarecoveredpercent = 30.0	# 30.0f
 
 	def __repr__(self):
-		return self.HeroArchetypeName
+		return self.heroarchetypename
 
+
+class_section_map = {
+		'RxHeroProvider': Archetype,
+		# 'RxSummonProvider': SummonProvider,
+		'RxSkillProvider': Skill,
+		'RxSkillUpgradeProvider': SkillUpgrade,
+		# 'RxPassiveUpgradeProvider': PassiveUpgrade,
+		# 'RxUpgradePathProvider': UpgradePath,
+	}
 
 def get_hero_config(file_name):
-	print("Parsing hero at {0}".format(file_name))
+	# print("Parsing hero at {0}".format(file_name))
 	config = configparser.ConfigParser(strict=False) # strict=False to allow duplicate keys within sections
 
 	# Stupid hackiness to decode utf-16le file, encode it as utf-8, then write to temporary file to fix BOM
@@ -86,7 +107,7 @@ def parse_hero(file_name):
 		raise ValueError()
 
 	sections = config.sections()
-	hero = Archetype()
+	hero = {'name': '', 'skills': [], 'skill_upgrades': []}
 
 	"""
 	Instantiate class object based on section name, names are like [ResourceID RxSkillProvider] where the string after
@@ -95,10 +116,21 @@ def parse_hero(file_name):
 	"""
 
 	for section in sections:
-		data = config[section]
-		if 'RxHeroProvider' in section: # Named like [Adept_Pawn_Arch RxHeroProvider]
-			resource_id = data['ResourceID']
-			hero.HeroArchetypeName = data['HeroArchetypeName']
+		res, _, res_type = section.partition(' ')
+		if len(res_type) <= 0:
+			continue
+		try:
+			Cls = class_section_map[res_type]
+		except KeyError:
+			continue
+
+		resource = Cls( **{key: val for key,val in config.items(section)} )
+		if isinstance(resource, Archetype):
+			hero['name'] = resource.heroarchetypename
+		elif isinstance(resource, Skill):
+			hero['skills'].append(resource)
+		elif isinstance(resource, SkillUpgrade):
+			hero['skills'].append(resource)
 
 	return hero
 
@@ -118,7 +150,7 @@ def parse_heroes():
 		except ValueError:
 			pass
 
-	print(heroes)
+	print(json.dumps(heroes))
 
 
 if __name__ == '__main__':
