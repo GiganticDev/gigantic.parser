c599531ead45d0dabeaf9b13f26613830e8b4451
Jordan Duty <djdduty@gmail.com>
2017-08-15 18:53:13 -0700

    Parse hero types, next up skills and skill upgrades

--- gigantic/parser/__main__.py
+++ gigantic/parser/__main__.py
@@ -1,20 +1,49 @@
 # encoding: utf-8
 
-
 import os
 import configparser
+import tempfile
 
 
 class Archetype(object):
 	def __init__(self, file_name):
 		print("Parsing hero at {0}".format(file_name))
-		self._config = configparser.ConfigParser(strict=False)
-		with open(file_name, 'rb') as f:
-			val = f.read().decode("utf-16le")
-			self._config.readfp(val.encode('ascii', 'ignore'))
-	
-		self._name = 'test' # self._config['RxHeroProvider']['HeroArchetypeName']
-	
+		self._config = configparser.ConfigParser(strict=False) # strict=False to allow duplicate keys within sections
+
+		# Stupid hackiness to decode utf-16le file, encode it as utf-8, then write to temporary file to fix BOM
+		u = open(file_name, "rb").read().decode("utf-16le").encode("utf-8")
+		fp = tempfile.TemporaryFile()
+		fp.write(u)
+		fp.seek(0)
+
+		# ConfigParser ultimately wants ascii
+		self._config.read_string(fp.read().decode("ascii", "ignore"))
+
+		self._hero_provider = {}
+		self._skill_providers = []
+		self._parse()
+
+	def _parse(self):
+		if not self._config:
+			raise NotImplementedError()
+
+		sections = self._config.sections()
+		for section in sections:
+			if 'RxHeroProvider' in section:
+				self._hero_provider = self._config[section]
+			elif 'RkSkillProvider' in section or 'RxSkillUpgradeProvider' in section:
+				self._skill_providers.append(self._config[section])
+
+		# print(self._skill_providers)
+		# print(self._hero_provider)
+
+		if not self._hero_provider:
+			raise ValueError()
+
+		self._name = self._hero_provider['HeroArchetypeName']
+
+		# print(sections)
+
 	def __repr__(self):
 		return self._name
 
@@ -24,13 +53,16 @@ def parse_heroes():
 	if len(hero_files) == 0:
 		print("Found no hero config files")
 		return
-	
+
 	heroes = []
 	os.chdir('Config/Heroes')
-	
+
 	for f in hero_files:
-		heroes.append(Archetype(f))
-	
+		try:
+			heroes.append(Archetype(f))
+		except ValueError:
+			pass
+
 	print(heroes)
 
 
